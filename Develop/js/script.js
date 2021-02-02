c50cd372cefc727c65075b6eb5069770bb5094ab
Your NameAlex <alehr45@gmail.com>
2021-02-01 18:05:31 -0600

    added search element

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,6 +1,21 @@
+var recipeSearchEl = document.querySelector("#recipe");
+var submitEl = document.querySelector(".submit");
+
+
+var formSumbitHandler = function (event) {
+  event.preventDefault();
+  if (recipe) {
+    pullData(recipe)
+   recipeSearchEl.value = "";
+  } else {
+    alert("Please enter a valid food item");
+  }
+ };
+
 pullData = function (data) {
+  var recipe = recipeSearchEl.value.trim();
     var apiKey = "fe4da658d0714157949fb5895d6363fb";
-    var apiUrl = "https://api.spoonacular.com/recipes/search?query=steak&apiKey=" + apiKey;
+    var apiUrl = "https://api.spoonacular.com/recipes/search?query=" + recipe + "&apiKey=" + apiKey;
     fetch(apiUrl)
       .then(function (response) {
         if (response.ok) {
@@ -11,5 +26,7 @@ pullData = function (data) {
       });
   
   };
-  pullData();
   
+  
+
+  submitEl.addEventListener("click", formSumbitHandler);
