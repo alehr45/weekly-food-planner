fda58ae06d5986cbfe6a64bc046c4e77b3a699d3
fthompson24 <fraserjthompson@gmail.com>
2021-02-02 23:45:56 -0600

    added calendar to page

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -28,20 +28,33 @@ pullData = function (data) {
 };
   
 function showRecipes(data) {
-    document.getElementById('recipe1').innerHTML = data.results[0].title.fontcolor("#9b96ff");
-    document.getElementById('recipe2').innerHTML = data.results[1].title.fontcolor("#9b96ff");
-    document.getElementById('recipe3').innerHTML = data.results[2].title.fontcolor("#9b96ff");
-    document.getElementById('recipe4').innerHTML = data.results[3].title.fontcolor("#9b96ff");
-    document.getElementById('recipe5').innerHTML = data.results[4].title.fontcolor("#9b96ff");
+  var searchUrl1 =  data.results[0].sourceUrl;
+  var searchUrl2 =  data.results[1].sourceUrl;
+  var searchUrl3 =  data.results[2].sourceUrl;
+  var searchUrl4 =  data.results[3].sourceUrl;
+  var searchUrl5 =  data.results[4].sourceUrl;
+  document.getElementById('recipe1').innerHTML = '<a href="' + searchUrl1 + '"target="_blank">' + data.results[0].title + " - Ready in " + data.results[0].readyInMinutes + " minutes" + '</a>';
+  document.getElementById('recipe2').innerHTML = '<a href="' + searchUrl2 + '"target="_blank">' + data.results[1].title + " - Ready in " + data.results[1].readyInMinutes + " minutes" + '</a>';
+  document.getElementById('recipe3').innerHTML = '<a href="' + searchUrl3 + '"target="_blank">' + data.results[2].title + " - Ready in " + data.results[2].readyInMinutes + " minutes" + '</a>';
+  document.getElementById('recipe4').innerHTML = '<a href="' + searchUrl4 + '"target="_blank">' + data.results[3].title + " - Ready in " + data.results[3].readyInMinutes + " minutes" + '</a>';
+  document.getElementById('recipe5').innerHTML = '<a href="' + searchUrl5 + '"target="_blank">' + data.results[4].title + " - Ready in " + data.results[4].readyInMinutes + " minutes" + '</a>';
 };
 
+
 $(function() {
-  $(".card").draggable( {
-    helper: "clone",
-    cursor: "move", cursorAt: { top: 60, left: 40 }
+  $(".content").draggable( {
+    appendTo: "body",
+    helper: "clone"
   });
+});
+
+let weekDay = moment()
+weekDay.subtract(weekDay.day()+1,"days")
+document.querySelectorAll(".box p").forEach(function(title) {
   
-})
+  //thisDay.add(-1,"days").format("ddd, MMM Do YY");
+  title.innerText = weekDay.add(1,"days").format("ddd, MMM Do YY");
+});
 
 submitEl.addEventListener("click", formSumbitHandler);
 
